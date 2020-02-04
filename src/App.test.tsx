b8c7193103c76d53e65d353c5afe19ad17357507
Ben Yorke <bendyorke@arya.local>
2020-02-04 10:08:07 +0100

    Initialize project

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+/* test('renders learn react link', () => { */
+/*   const { getByText } = render(<App />) */
+/*   const linkElement = getByText(/learn react/i) */
+/*   expect(linkElement).toBeInTheDocument() */
+/* }) */
+
+test.skip('fires purchase events on load', () => {
+
+})
+
+test.skip('logs the last purchase event on screen', () => {
+
+})
+
+test.skip('graphs purchase events', () => {
+
+})
+
+test.skip('can change the rate of events', () => {
+
+})
+
+test.skip('can change the probability of events', () => {
+
+})
